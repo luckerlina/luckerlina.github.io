618dbbc17d445a14bba76133dad8ae7350c40241
luckerlina <112793018+luckerlina@users.noreply.github.com>
2022-10-01 06:58:03 +0000

    🎉 V2.0.0 更改全站点名称为 六月的风 JuneWind junewind Updated By Github Actions

--- js/randombg.js
+++ js/randombg.js
@@ -1,7 +1,1 @@
-// see https://akilar.top/posts/ebf20e02/
-//随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
-var backimg = ['url(/img/background/home/bg1.webp)', 'url(/img/background/home/bg10.webp)', 'url(/img/background/home/bg14-car.webp)', 'url(/img/background/home/bg2.webp)', 'url(/img/background/home/bg3.webp)', 'url(/img/background/home/bg4-g.webp)', 'url(/img/background/home/bg4.webp)', 'url(/img/background/home/bg5-g.webp)', 'url(/img/background/home/bg5.webp)', 'url(/img/background/home/bg6.webp)', 'url(/img/background/home/bg7.webp)', 'url(/img/background/home/bg8.webp)', 'url(/img/background/home/bg9.webp)', 'url(/img/background/home/bgblue1.webp)', 'url(/img/background/home/bgc15-g.webp)', 'url(/img/background/home/bgc9.webp)']
-//获取背景图片总数，生成随机数
-var bgindex =Math.floor(Math.random() * backimg.length);
-//重设背景图片
-document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
+var backimg=["url(/img/background/home/bg1.webp)","url(/img/background/home/bg10.webp)","url(/img/background/home/bg14-car.webp)","url(/img/background/home/bg2.webp)","url(/img/background/home/bg3.webp)","url(/img/background/home/bg4-g.webp)","url(/img/background/home/bg4.webp)","url(/img/background/home/bg5-g.webp)","url(/img/background/home/bg5.webp)","url(/img/background/home/bg6.webp)","url(/img/background/home/bg7.webp)","url(/img/background/home/bg8.webp)","url(/img/background/home/bg9.webp)","url(/img/background/home/bgblue1.webp)","url(/img/background/home/bgc15-g.webp)","url(/img/background/home/bgc9.webp)"],bgindex=Math.floor(Math.random()*backimg.length);document.getElementById("web_bg").style.backgroundImage=backimg[bgindex];
--- js/rightside_share.js
+++ js/rightside_share.js
@@ -1,6 +1,1 @@
-// 分享本页
-function share() {
-    let url = window.location.origin + window.location.pathname
-    new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
-    btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
-}
+function share(){let n=window.location.origin+window.location.pathname;new ClipboardJS(".share",{text:function(){return"标题："+document.title+"\n链接："+n}}),btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")}
